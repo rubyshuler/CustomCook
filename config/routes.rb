94944ddca3b4ac3ff4aeb7e9b943122d2a487603
Ruby-Shuler <stasia.shuler@gmail.com>
2018-09-25 14:23:37 +0300

    Модели

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :steps
+  resources :dishes
+  resources :ingredients
+  resources :recipe_ingredients
+  resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
