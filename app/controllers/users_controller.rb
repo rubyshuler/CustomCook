c54f3614c6b5acceeb4f48b7107d6036a0dec943
rubyshuler <stasiashuler@o-din.ru>
2019-06-25 09:28:33 +0300

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
 
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
                                         :following, :followers]
